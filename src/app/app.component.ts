499b838109ee813ce730ae228f1f326db136709d
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-03-31 17:06:51 +0530

    made Front text and animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,46 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   ishover: boolean = false;
   title = 'Hello';
+
+  theme = 'light';
+  themeTitle = this.theme.toUpperCase();
+  sunMoon = 'bi bi-brightness-high';
+  isDark = false;
+  bodyText = ['Hello', 'I am', 'Ritesh'];
   ngOnInit(): void {}
+  getTheme() {
+    if (this.isDark) {
+      this.theme = 'dark';
+      this.themeTitle = this.theme.toUpperCase();
+      this.sunMoon = 'bi bi-moon-fill';
+    } else {
+      this.theme = 'light';
+      this.themeTitle = this.theme.toUpperCase();
+      this.sunMoon = 'bi bi-brightness-high';
+    }
+  }
+  getValue(event: any, id: number) {
+    if (event.type === 'mouseover') {
+      if (id === 0) {
+        this.bodyText[id] = 'About';
+      }
+      if (id === 1) {
+        this.bodyText[id] = 'Work';
+      }
+      if (id === 2) {
+        this.bodyText[id] = 'Contact';
+      }
+    }
+    if (event.type === 'mouseout') {
+      if (id === 0) {
+        this.bodyText[id] = 'Hello.';
+      }
+      if (id === 1) {
+        this.bodyText[id] = 'I am';
+      }
+      if (id === 2) {
+        this.bodyText[id] = 'Ritesh';
+      }
+    }
+  }
 }
