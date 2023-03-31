72ae4d1c08f283c93dada260a357d281deb70f18
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-03-31 10:31:57 +0530

    created files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'portfolio-day-night';
+export class AppComponent implements OnInit {
+  ishover: boolean = false;
+  title = 'Hello';
+  ngOnInit(): void {}
 }
