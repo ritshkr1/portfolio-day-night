955e25521e3b904acafcf6a20dd08798d898271c
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-04-10 14:53:18 +0530

    Work page working now.

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.css'],
+  styleUrls: ['./work.component.css', '../about/about.component.css'],
 })
 export class WorkComponent {}
